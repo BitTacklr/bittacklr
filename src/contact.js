1e82bc26911fc6c81b7378328561e7491c149e23
Yves Reynhout <yves.reynhout@gmail.com>
2017-05-07 17:48:20 +0200

    Added email, google plus and youtube, and first attempt at making it work on windows (pandoc not working yet).

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,6 @@
 var smallEmailLink = document.getElementById("smallemaillink");
-if (smallEmailLink !== undefined)
+if (smallEmailLink != undefined)
+{
     smallEmailLink.setAttribute("href", "mailto:info@bittacklr.be");
+    smallEmailLink.innerText = "info@bittacklr.be";
+}
