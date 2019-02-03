03ea7c58548cecf65f2d5eb4e790516cf1816a73
Yves Reynhout <info@bittacklr.be>
2019-02-03 14:48:00 +0100

    Updated with new training

--- run-local.sh
+++ run-local.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 mono packages/FAKE/tools/FAKE.exe build.fsx
 echo 'Building docker image'
-sudo docker build -t bittacklr-image:dev .
+docker build -t bittacklr-image:dev .
 echo 'Running docker container based on image'
-sudo docker run --name bittacklrdev -d -p 80:80 bittacklr-image:dev
+docker run --name bittacklrdev -d -p 80:80 bittacklr-image:dev
 echo 'Press enter to exit'
 read -s -n 1 key
 echo 'Stopping docker container'
-sudo docker stop bittacklrdev
+docker stop bittacklrdev
 echo 'Removing docker container'
-sudo docker rm bittacklrdev
+docker rm bittacklrdev
 echo 'Removing docker image'
-sudo docker rmi bittacklr-image:dev
+docker rmi bittacklr-image:dev
